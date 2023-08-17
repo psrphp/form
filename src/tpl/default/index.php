3500987b2e0216c4f5d0430b2a0130aa88ccfd65
John Doe <johndoe@example.com>
2023-08-17 14:50:14 +0800

    1

--- src/tpl/default/index.php
+++ src/tpl/default/index.php
@@ -5,20 +5,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{$title ?: '表单'}</title>
+    <style>
+        a {
+            text-decoration: none;
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+
+        table th,
+        table td {
+            border: 1px solid #ddd;
+            padding: 5px;
+        }
+
+        fieldset {
+            border-width: 1px;
+            border-color: #888;
+        }
+
+        fieldset legend {
+            font-weight: bold;
+        }
+
+        select,
+        input,
+        textarea {
+            padding: 2px;
+        }
+    </style>
 </head>
 
 <body>
-    <div>
-        <form action="{$action?:''}" method="{$method?:'POST'}" enctype="{$options['enctype']??'application/x-www-form-urlencoded'}" id="form">
-            <h1>{$title?:'表单'}</h1>
-            <div style="display: flex;flex-direction: column;gap: 10px;">
-                {echo $body}
-            </div>
-            <div style="margin-top: 20px;">
-                <button type="submit">{$options['submit'] ?? '提交'}</button>
-            </div>
-        </form>
-    </div>
+    <form action="{$action?:''}" method="{$method?:'POST'}" enctype="{$options['enctype']??'application/x-www-form-urlencoded'}" id="form">
+        <h1>{$title?:'表单'}</h1>
+        <div style="display: flex;flex-direction: column;gap: 10px;">
+            {echo $body}
+        </div>
+        <div style="margin-top: 20px;">
+            <button type="submit">{$options['submit'] ?? '提交'}</button>
+        </div>
+    </form>
 
     <script>
         (function() {
@@ -37,7 +65,7 @@
                 xmlhttp.responseType = "json";
                 xmlhttp.onerror = function(e) {};
                 xmlhttp.ontimeout = function(e) {
-                    alert("Timeout!!");
+                    alert("超时");
                 };
                 xmlhttp.onreadystatechange = function() {
                     if (xmlhttp.readyState == 4) {
